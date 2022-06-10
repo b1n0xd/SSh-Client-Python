4eafe972d0df449544feb9a7cb83c878342a49ca
Luciano Vieira <contato@lucianovieirapro.com>
2022-06-10 10:08:36 -0300

    Initial commit

---
+++ sshconnect.py
@@ -0,0 +1,21 @@
+from pexpect import pxssh
+import getpass
+try:
+    s = pxssh.pxssh()
+    hostname = input('hostname: ')
+    username = input('username: ')
+    password = getpass.getpass('password: ')
+    s.login(hostname, username, password)
+    s.sendline('uptime')
+    s.prompt()
+    print(s.before)
+    s.sendline('ls -l')
+    s.prompt()
+    print(s.before)
+    s.sendline('df')
+    s.prompt()
+    print(s.before)
+    s.logout()
+except pxssh.ExceptionPxssh as e:
+    print("Failed login...")
+    print(e)
